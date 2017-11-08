34c4a13a66bd69ce1d4e5ed39bea4b57e884610c
trwiley <taylor.r.wiley@gmail.com>
2017-11-08 11:32:32 -0500

    Added database entities.

--- site/ArchyMap/map_page/models.py
+++ site/ArchyMap/map_page/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#ID's are automatically generated.
+
+class Admin(models.Model):
+    username = models.CharField(max_length = 20)
+    pw = models.CharField(max_length = 40)
+
+class Artifact(models.Model):
+    name = models.CharField(max_length = 255)
+    siteno = models.CharField(max_length = 10)
+    state = models.CharField(max_length = 255)
+    county = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 1000)
+    admin = models.ForeignKey(Admin)
