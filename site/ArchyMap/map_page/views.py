1da8318472e60f18d47f98278a788f24e581678a
Michael Barney <mbarneyjr5309@gmail.com>
2017-11-30 15:53:44 -0500

    Gets data out of database

--- site/ArchyMap/map_page/views.py
+++ site/ArchyMap/map_page/views.py
@@ -2,15 +2,22 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django import template
-
+from map_page.models import Artifact, Admin
 # Create your views here.
 
 def map_view(request):
     return render(request, 'map/map.html')
 
 def get_data(request, *args, **kwargs):
-    data = [{
-        "lat": 41,
-        "lng": -86,
-    }]
+    data = []
+    artifacts = Artifact.objects.all()
+    for artifact in artifacts:
+        data.append({
+            "lat": float(artifact.latitude),
+            "lng": float(artifact.longitude),
+        })
+    # data = [{
+    #     "lat": 41,
+    #     "lng": -86,
+    # }]
     return JsonResponse(data, safe=False)
